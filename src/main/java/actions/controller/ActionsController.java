26ae541f458add3aef1e217f27457b2f32ebe402
yhy0009 <ref0009@naver.com>
2025-06-02 10:27:32 +0900

    create project

---
+++ src/main/java/actions/controller/ActionsController.java
@@ -0,0 +1,19 @@
+package actions.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ActionsController {
+	
+	@GetMapping(value = "/api/actions")
+	public String actionsTest() {
+		return "app-with-actions version:0.0.2";
+	}
+	
+	@GetMapping(value = "/")
+	public String actionsTest1() {
+		return "app-with-actions version";
+	}
+	
+}
